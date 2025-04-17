05f175542f4064af54772e5cd305081ec0ac116d
Dominik Opyd <dominik.opyd@gmail.com>
2025-04-17 14:26:34 +0200

    refactor: restore ai tool

--- server/routes/sse.ts
+++ server/routes/sse.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 
 export default defineEventHandler(async (event) => {
   // @ts-expect-error globalThis.__env__ is not defined
-  // const ai = process.env.AI || globalThis.__env__?.AI || globalThis.AI
+  const ai = process.env.AI || globalThis.__env__?.AI || globalThis.AI
   const res = event.node.res
 
   const MCPServer = new McpServer({
@@ -14,51 +14,19 @@ export default defineEventHandler(async (event) => {
     capabilities: {},
   })
 
-  MCPServer.resource(
-    'config',
-    'config://app',
-    async uri => ({
-      contents: [{
-        uri: uri.href,
-        text: 'App configuration here',
-      }],
-    }),
+  MCPServer.tool(
+    'aiSearch',
+    { message: z.string() },
+    async ({ message }) => {
+      const data = await ai.autorag('craig').aiSearch({ query: message })
+
+      return {
+        content: [{
+          type: 'text', text: data.response,
+        }],
+      }
+    },
   )
-
-  MCPServer.tool('add',
-    { a: z.number(), b: z.number() },
-    async ({ a, b }) => ({
-      content: [{ type: 'text', text: String(a + b) }],
-    }),
-  )
-
-  MCPServer.prompt(
-    'review-code',
-    { code: z.string() },
-    ({ code }) => ({
-      messages: [{
-        role: 'user',
-        content: {
-          type: 'text',
-          text: `Please review this code:\n\n${code}`,
-        },
-      }],
-    }),
-  )
-
-  // MCPServer.tool(
-  //  'aiSearch',
-  //  { message: z.string() },
-  //  async ({ message }) => {
-  //    const data = await ai.autorag('craig').aiSearch({ query: message })
-
-  //    return {
-  //     content: [{
-  //        type: 'text', text: data.response,
-  //     }],
-  //   }
-  // },
-  // )
 
   const transport = new SSEServerTransport('/message', res)
 
